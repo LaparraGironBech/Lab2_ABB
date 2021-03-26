c1854f15420e2ca93f7adb0d5ffcc391b9ad2918
JasonZamora <JasonZamora@users.noreply.github.com>
2021-03-25 23:47:39 -0600

    Base2

--- ListaArtesanal/Controllers/MedicinaController.cs
+++ ListaArtesanal/Controllers/MedicinaController.cs
@@ -30,7 +30,7 @@ namespace ListaArtesanal.Controllers
         }
         public IActionResult IndexVer()
         {
-            return View(ListMedicina);
+            return View();
         }
 
         [HttpPost]
